9c10a57c63725092aac4ffe178245f4eb9ebfe81
Eloic Rakoto-Arisoa <eloicmax994@gmail.com>
2025-03-06 09:52:53 +0300

    New changes
    update server.js
    update vercel.js

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Désactive le buffering des commandes MongoDB
+mongoose.set('bufferCommands', false);
+
 // Connexion à MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connecté"))
-  .catch(err => console.log(err));
+})
+  .then(() => console.log("MongoDB connecté"))
+  .catch(err => {
+    console.error("Erreur de connexion à MongoDB :", err);
+    process.exit(1); // Quitte l'application en cas d'erreur
+  });
 
 // Routes
 app.use('/', require('./routes/testRoutes'));
